9cd4c59fbe8046b7d29071d57cbc8bfe38bddecd
Rjsfgjnvsnkk <166946285+Rjsfgjnvsnkk@users.noreply.github.com>
2025-08-07 16:55:08 +0530

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,84 @@
+import { Cinzel } from "next/font/google";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // your paths
+    "./src/**/*.{ts,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily:{
+        Cinzel:['Cinzel' , 'serif'],
+      },
+      screens:{
+        'xs': '300px'
+      },
+      animation: {
+        aurora: "aurora 60s linear infinite",
+        gradient: "gradient 3s linear infinite",
+        scroll:
+        "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+        marquee: "marquee var(--duration) linear infinite",
+        "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
+      },
+      keyframes: {
+        "shine-pulse": {
+          "0%": {
+            "background-position": "0% 0%",
+          },
+          "50%": {
+            "background-position": "100% 100%",
+          },
+          to: {
+            "background-position": "0% 0%",
+          },
+        },
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
+        gradient: {
+          '0%': { backgroundPosition: '0% 0%' },
+    '100%': { backgroundPosition: '100% 0%' },
+        },
+        marquee: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(-100% - var(--gap)))" },
+        },
+        "marquee-vertical": {
+          from: { transform: "translateY(0)" },
+          to: { transform: "translateY(calc(-100% - var(--gap)))" },
+        },
+      
+      },
+    },
+  },
+  plugins: [addVariablesForColors],
+};
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
+
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
